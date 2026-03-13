35c1280cf25da7697bf6998d64c01a4ff5d17cb6
Tyler <101429436+Opuqide@users.noreply.github.com>
2026-03-12 21:51:02 -0500

    blelaertgagliuhderatgiu

--- src/_templates/_head.php
+++ src/_templates/_head.php
@@ -1,5 +1,5 @@
 <?php
-  $URL = "https://alexflipnote.dev";
+  $URL = "https://beta.opuqide.dev";
 ?>
 
 <title><?php echo $title ?? "default title"; ?></title>
@@ -8,20 +8,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 <meta name="theme-color" content="#14bae4"/>
 <link rel="icon" type="image/x-icon" href="/static/images/favicon.ico"></link>
-<meta property="og:image" content="/static/images/branding/avatar.png" />
+<meta property="og:image" content="/static/images/Amy.png" />
 
 <meta content="<?php echo $title ?? "Title"; ?>" property="og:title"/>
 <meta content="<?php echo $description ?? "default description"; ?>" property="og:description"/>
 <meta content="<?php echo $description ?? "default description"; ?>" name="description"/>
 
 <meta name="revisit-after" content="2 days"/>
-<meta name="keywords" content="<?php echo $keywords ?? "AlexFlipnote, Twitter, Developer, Github"; ?>"/>
+<meta name="keywords" content="<?php echo $keywords ?? "Opuqide, Twitter, Developer, Github"; ?>"/>
 <meta property="og:locale" content="en_GB"/>
 <link rel="canonical" href="<?php echo "{$URL}{$path}"; ?>" />
 <meta property="og:url" content="<?php echo "{$URL}{$path}"; ?>" />
-<meta property="og:site_name" content="AlexFlipnote"/>
+<meta property="og:site_name" content="Opuqide"/>
 <meta name="twitter:card" content="summary"/>
-<meta name="twitter:site" content="@AlexFlipnote"/>
+<meta name="twitter:site" content="@Opuqide"/>
 
 <!-- CSS -->
 <link href="/static/css/modesta.css" type="text/css" rel="stylesheet">
